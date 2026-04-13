60a8a6f19b7026817bc685f304085bf682aabc8c
Makosha28 <st10534919@gmail.com>
2026-04-13 14:19:07 +0200

    Refactor LoginTest by removing boilerplate code
    Removed unnecessary boilerplate code and updated author name.

--- LoginTest.java
+++ LoginTest.java
@@ -5,38 +5,20 @@
 package com.mycompany.main;
 
 import java.util.Scanner;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  *
- * @author mamphekgonthabiseng
+ * @author mamphekgomakosha
  */
 public class LoginTest {
     
     public LoginTest() {
     }
     
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
-    @BeforeEach
-    public void setUp() {
-    }
-    
-    @AfterEach
-    public void tearDown() {
-    }
-
+   
     /**
      * Test of loginUser method, of class Login.
      */
